fef6521a0132f63018b61bacb643969829ad2205
fiddleswe <smartsoodprasert69@gmail.com>
2025-09-16 17:58:57 +0700

    refactor: rename command from ping.js -> test.js

--- discord-bot/commands/test.js
+++ discord-bot/commands/test.js
@@ -2,9 +2,9 @@ const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
-		.setName('ping')
-		.setDescription('Replies with Pong!'),
+		.setName('test')
+		.setDescription('Test command!'),
 	async execute(interaction) {
-		await interaction.reply('Pong!');
+		await interaction.reply('Hello World!');
 	},
 };
